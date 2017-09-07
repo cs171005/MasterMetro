02b85f215faa6e13e4a298dd59164244f238b400
Y.Kono <ev30112@meiji.ac.jp>
2017-09-07 12:04:57 +0900

    enable to simulate traffic incident scenes.

--- main.py
+++ main.py
@@ -39,12 +39,22 @@ for i in range(0,len(TrainList)):
 	if (TrainList[i].TimeTable[0] - datetime.timedelta(seconds=30)).time() < current.time():
 		firstTrainNum += 1
 
+trouble = True
+troubleStart = datetime.datetime(100,1,1,7,30,00)
+troubleEnd = datetime.datetime(100,1,1,7,45,00)
+arrive = datetime.datetime(100,1,1,7,30,00) #dummy init
 while current <= end:
 	for i in range(firstTrainNum,len(TrainList)):
-		if TrainList[i].CurrentStop < StationNum-1: #Halfway
+		#Incident 
+		if trouble:
+			if troubleStart <= current <= troubleEnd:
+				break
+			
+		if TrainList[i].CurrentStop < StationNum-1: #Starting and Halfway statitons
 			# ARRIVE
-			if TrainList[i].CurrentStop != -1 and current.time() > TrainList[i].TimeTable[TrainList[i].CurrentStop*2-1].time():
+			if TrainList[i].CurrentStop != -1 and current.time() > TrainList[i].TimeTable[TrainList[i].CurrentStop*2-1].time() and TrainList[i].CurrentSite == CellNumPerMinute*interStationMinutes*TrainList[i].CurrentStop:
 				TrainList[i].isForward = False
+				arrive = current
 			elif TrainList[i].CurrentStop == -1 and current.time() >= (TrainList[i].TimeTable[0] - datetime.timedelta(seconds=30)).time():
 			#starting depot to starting station
 					TrainList[i].CurrentStopUpdate()
@@ -52,6 +62,7 @@ while current <= end:
 					TrainList[i].ConvertOperationMode()	#F->T
 					TrainList[i].isForward = False
 					LineState.state[TrainList[i].CurrentSite].existTrain = True
+					arrive = current
 			# DEPERTURE (updating destination station)
 			if TrainList[i].CurrentStop == -1 and current.time() >= TrainList[i].TimeTable[0].time():
 			#starting depot to starting station
@@ -66,6 +77,7 @@ while current <= end:
 				if LineState.state[TrainList[i].CurrentSite+1].existTrain == False: #Collision prevention
 					TrainList[i].CurrentStopUpdate()
 					TrainList[i].isForward = True
+		
 		elif TrainList[i].CurrentStop == StationNum-1: #terminal station
 		# ARRIVE
 			if TrainList[i].CurrentStop != -1 and current.time() > TrainList[i].TimeTable[TrainList[i].CurrentStop*2-1].time():
@@ -79,14 +91,14 @@ while current <= end:
 	  			TrainList[i].ConvertOperationMode() #T->F
 				TrainList[i].isForward = False
 			
-		if TrainList[i].InOperation and TrainList[i].isForward and TrainList[i].CurrentSite != 999:
 		#move to the next site cell (represents an inter-section)
+		if TrainList[i].InOperation and TrainList[i].isForward and TrainList[i].CurrentSite != 999:
 			if LineState.state[TrainList[i].CurrentSite+1].existTrain == False: #Collision prevention
 				LineState.state[TrainList[i].CurrentSite].existTrain = False
 				TrainList[i].CurrentSiteUpdate()
 				LineState.state[TrainList[i].CurrentSite].existTrain = True
 
-	#print current.time(),TrainList[0].TrainNum,TrainList[0].CurrentStop,TrainList[0].CurrentSite,TrainList[0].isForward,TrainList[0].InOperation
+	print current.time(),TrainList[0].TrainNum,TrainList[0].CurrentStop,TrainList[0].CurrentSite,TrainList[0].isForward,TrainList[0].InOperation
 	LineState.OutputState(current)
 	current += timePerStep
 
