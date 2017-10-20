2ef74640c179d35eae9a6e528df52af38966916b
Y.Kono <cs171005@meiji.ac.jp>
2017-10-20 20:10:45 +0900

    - added MasterMetroViewer.pde. - changed log file character (1->■) - changed the directory at which log file is genearted.

--- LineState.py
+++ LineState.py
@@ -21,15 +21,16 @@ class LineState:
 			if Site.isStation:
 				output += '['
 			if Site.existTrain:
-				output += '1'
+				output += '■'
 			else:
-				output += '0'
+				output += '□'
 			if Site.isStation:
 				output += ']'
 		print currentTime.time(),output
 
 	def OutputState(self,currentTime):
 		#self.outputFile = 'output'+'.txt'
+		os.chdir("/Users/ev30112/Dropbox/programming/MasterMetro/MasterMetroViewer/data")
 		self.outputFile = 'result-'+datetime.datetime.now().strftime('%y%m%d-%H%M%S')+'.txt'
 
 		if os.path.exists(self.outputFile):
@@ -42,9 +43,9 @@ class LineState:
 			if Site.isStation:
 				output += '['
 			if Site.existTrain:
-				output += '1'
+				output += '■'
 			else:
-				output += '0'
+				output += '□'
 			if Site.isStation:
 				output += ']'
 		f.write(str(currentTime.time())+' '+output+'\n')
@@ -52,7 +53,6 @@ class LineState:
 		f.close()
 
 class SiteCell:
-
 	def __init__(self,isStation,segmentationNumber,stationNumber,existTrain):
 		self.existTrain = existTrain
 		self.segmentationNumber = segmentationNumber
---
+++ MasterMetroViewer/MasterMetroViewer.pde
@@ -0,0 +1,36 @@
+BufferedReader reader;
+String line;
+ 
+void setup() {
+  // make an instance of File class to access data files with BufferedReader 
+  File dir = new File(sketchPath());
+  
+  // Open the file from the createWriter() example
+  reader = createReader("result-171020-194114.txt");   
+  size(displayWidth,200);
+  background(255);
+  fill(0);
+  frameRate(40);
+}
+ 
+void draw() {
+  background(255);
+  try {
+    line = reader.readLine();
+  } catch (IOException e) {
+    e.printStackTrace();
+    line = null;
+  }
+  if (line == null) {
+    // Stop reading because of an error or file is empty
+    noLoop();  
+  } else {
+    //println(line);
+    String[] part = split(line, ' ');
+    //println(part[1]);
+    textSize(10);
+    text(part[0], 10, 10);
+    textSize(8.8);
+    text(part[1], 2, 100);
+  }
+} 
