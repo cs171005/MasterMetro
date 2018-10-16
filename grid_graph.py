da8456bc734bd731a4b04817e6a2b0a09734d070
Y.Kono <cs171005@meiji.ac.jp>
2018-10-16 14:31:50 +0900

    - deleted unnecessary components.

--- grid_graph.py
+++ grid_graph.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from TrafficFlowSimulator import *
 
+num_of_station = 19
+num_of_train = 85
+
 TFS = TrafficFlowSimulator()
 ttt = TFS.runWithDelayMatrix()
 
@@ -17,7 +20,6 @@ def add_cross_edge(gp, shape):
         nx_node = (node[0] - 1, node[1] + 1)
         if 0 <= nx_node[0] and nx_node[1] <= shape[1]-1 :
             gp.add_edge(node, nx_node)
-
 def remove_vertical_edge(gp, shape):
     # print gp.edges()
     for row in range(1,shape[1]):
@@ -26,7 +28,6 @@ def remove_vertical_edge(gp, shape):
             origin = (col,row)
             dest = (col,row-1)
             gp.remove_edge(origin, dest)
-
 def remove_horizontal_edge(gp, shape):
     # print gp.edges()
     for row in range(0,shape[1]):
@@ -36,17 +37,7 @@ def remove_horizontal_edge(gp, shape):
             dest = (col-1,row)
             gp.remove_edge(origin, dest)
 
-
-
-ngrid = 5
-
-# num_of_station = 19
-# num_of_train = 40
-
-num_of_station = 19
-num_of_train = 85
-
-
+# compose PERT diagram
 g_dim = [1+2*(num_of_station-1), num_of_train]
 
 gp = nx.grid_2d_graph(1+2*(num_of_station-1), num_of_train)
@@ -97,7 +88,7 @@ nx.set_edge_attributes(gp, -1*ddd, 'negative_weight')
 
 criterior = 180
 bolttt = TFS.runWithDelayDissatisfactionBool(criterior)
-print len(bolttt[0]),len(bolttt)
+# print len(bolttt[0]),len(bolttt)
 
 for eg in gp.edges(data = True):
     if eg[0] in cost_dict:
@@ -112,26 +103,19 @@ for eg in gp.edges(data = True):
     modified_p = 11.0
     if ((eg[0][1] < len(bolttt) and eg[0][0] < 36) and bolttt[eg[0][1]][eg[0][0]]):
         if ((eg[1][1] < len(bolttt) and eg[1][0] < 36) and bolttt[eg[1][1]][eg[1][0]]):
-            # a:true,b:true
+            # eg[0]:true, eg[1]:true
             gp.node[eg[0]]['color'] = 'green'
             gp.node[eg[1]]['color'] = 'green'
             eg[2]['weight'] += modified_p * 2
         else:
-            # a:true,b:false
+            # eg[0]:true, eg[1]:false
             gp.node[eg[0]]['color'] = 'green'
             eg[2]['weight'] += modified_p
     elif(((eg[1][1] < len(bolttt) and eg[1][0] < 36) and bolttt[eg[1][1]][eg[1][0]])):
-        # a:false,b:true
+        # eg[0]:false, eg[1]:true
         gp.node[eg[1]]['color'] = 'green'
         eg[2]['weight'] += modified_p
 
-        '''
-        for n in gp.nodes():
-            print n,
-            if n[0] < 36 and bolttt[n[0]][n[1]]:
-                print n, bolttt[n[0]][n[1]]
-                gp.node[n]['color'] = 'pink'
-        '''
     print  str(eg[0]) + " → " + str(eg[2]['weight']) + " → " + str(eg[1])
 
 for eg in gp.edges(data = True):
@@ -139,13 +123,11 @@ for eg in gp.edges(data = True):
         if eg[1] in cost_dict[eg[0]]:
             eg[2]['negative_weight'] = -1*cost_dict[eg[0]][eg[1]]
 
-idcs = np.random.choice(len(gp.nodes()), int(ngrid * ngrid * 0.2), replace=False)
+idcs = np.random.choice(len(gp.nodes()), int(5 * 5 * 0.2), replace=False)
 # print idcs
 
-
-
 # スタート・ゴール・障害物を設定する
-st, gl, obs = (0,0), (2,2), [list(gp.nodes())[i] for i in idcs[2:]]
+st, gl, obs = (0,0), (30,60), [list(gp.nodes())[i] for i in idcs[2:]]
 # st, gl, obs = (0,0), (np.random.randint(g_dim[0]),np.random.randint(g_dim[1])), [list(gp.nodes())[i] for i in idcs[2:]]
 obs = [list(gp.nodes())[i] for i in idcs[2:]]
 
@@ -161,54 +143,6 @@ def cost(a, b):
 
 
     return dist + penalty
-
-def cost2(a, b):
-    """
-    コスト関数
-    """
-
-    x1 = np.array(a, dtype=np.float32)
-    x2 = np.array(b, dtype=np.float32)
-    dist = np.linalg.norm(x1 - x2)
-    dist = -dist
-    # print "the cost between" +  + "is" +
-    penalty = 0.0
-    if ((a[1] < len(bolttt) and a[0] < 36) and bolttt[a[1]][a[0]]):
-        if ((b[1] < len(bolttt) and b[0] < 36) and bolttt[b[1]][b[0]]):
-            # a:true,b:true
-            gp.node[a]['color'] = 'green'
-            gp.node[b]['color'] = 'green'
-            penalty = 1.0 * 2
-        else:
-            # a:true,b:false
-            gp.node[a]['color'] = 'green'
-            penalty = 1.0
-    elif(((b[1] < len(bolttt) and b[0] < 36) and bolttt[b[1]][b[0]])):
-        # a:false,b:true
-        gp.node[b]['color'] = 'green'
-        penalty = 1.0
-
-        '''
-        for n in gp.nodes():
-            print n,
-            if n[0] < 36 and bolttt[n[0]][n[1]]:
-                print n, bolttt[n[0]][n[1]]
-                gp.node[n]['color'] = 'pink'
-        '''
-    # print a[0],a[1]
-
-    '''
-    if a[0]%2 == 0:
-        penalty = claim_file[(a[0]/2)]["arrival_delay"]
-    else:
-        penalty = claim_file[((a[0]/2)+1)]["deperture_delay"]
-
-    '''
-    # gp.node[a]['color'] = 'green'
-    # penalty = 1.0e6
-    # print "normal penalty is here."
-    return dist - penalty
-
 
 def dist(a, b):
     """
@@ -239,12 +173,6 @@ for nd in path2[st][gl][:]:
         # print ('{0}-[{1}]→{2}'.format(previous_nd,cost_dict[previous_nd][nd],nd))
     previous_nd = nd
 
-# print longest_length
-
-# path2 = nx.astar_path(gp, st, gl, cost2)
-# length2 = nx.astar_path_length(gp, st, gl, cost2)
-
-
 print(path)
 print(length)
 
@@ -267,12 +195,7 @@ gp.node[st]['color'] = 'green'
 gp.node[gl]['color'] = 'red'
 for o in obs:
     gp.node[o]['color'] = 'black'
-'''
-for n in gp.nodes():
-    if n[0] < 36 and bolttt[n[1]][n[0]]:
-        print n, bolttt[n[1]][n[0]]
-        gp.node[n]['color'] = 'pink'
-'''
+
 #show the graph
 pos = dict((n, n) for n in gp.nodes())
 nx.draw(gp,
